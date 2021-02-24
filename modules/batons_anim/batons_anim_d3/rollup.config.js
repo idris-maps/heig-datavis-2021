b17de3e9333df1cf046393c4a4094474691347c8
Anders <idris.maps@gmx.ch>
2021-02-24 23:18:48 +0100

    Anim d3

---
+++ modules/batons_anim/batons_anim_d3/rollup.config.js
@@ -0,0 +1,44 @@
+import commonjs from '@rollup/plugin-commonjs'
+import resolve from '@rollup/plugin-node-resolve'
+import livereload from 'rollup-plugin-livereload'
+import { terser } from 'rollup-plugin-terser'
+
+const production = !process.env.ROLLUP_WATCH
+
+function serve() {
+  let server
+  function toExit() {
+    if (server) server.kill(0)
+  }
+  return {
+    writeBundle() {
+      if (server) return
+      server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+        stdio: ['ignore', 'inherit', 'inherit'],
+        shell: true
+      })
+      process.on('SIGTERM', toExit)
+      process.on('exit', toExit)
+    }
+  }
+}
+
+export default {
+  input: 'src/index.js',
+  output: {
+    format: 'iife',
+    file: 'public/build/bundle.js'
+  },
+  plugins: [
+    resolve({
+      browser: true,
+    }),
+    commonjs(),
+    !production && serve(),
+    !production && livereload('public'),
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
+}
