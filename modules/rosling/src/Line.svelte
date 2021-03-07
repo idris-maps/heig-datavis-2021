df43b94aa8e1f825be7cf9355f84676b2d68d9b3
Anders <idris.maps@gmx.ch>
2021-03-07 19:56:11 +0100

    Add line

---
+++ modules/rosling/src/Line.svelte
@@ -0,0 +1,19 @@
+<script>
+  import {
+    getLineColorByName,
+    getLinePathByName,
+    MARGIN_LEFT,
+    MARGIN_TOP,
+  } from './utils'
+  
+  export let hovered
+
+  $: d = getLinePathByName(hovered)
+  $: stroke = getLineColorByName(hovered)
+</script>
+
+<g transform={`translate(${MARGIN_LEFT}, ${MARGIN_TOP})`}>
+  <path d={d} stroke="white" fill="none" stroke-width="4" />
+  <path d={d} stroke={stroke} fill="none" stroke-width="2" />
+</g>
+
