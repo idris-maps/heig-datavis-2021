de6a498532e469004b5125772af1909ea601eac5
Anders <idris.maps@gmx.ch>
2021-03-12 11:41:04 +0100

    Fix typo

--- modules/exercice_rest/run.js
+++ modules/exercice_rest/run.js
@@ -1,5 +1,5 @@
 const R = require('ramda')
-const fetch = require('fetch')
+const fetch = require('node-fetch')
 
 const URL_USERS = 'https://jsonplaceholder.typicode.com/users'
 const URL_POSTS = 'https://jsonplaceholder.typicode.com/posts'
