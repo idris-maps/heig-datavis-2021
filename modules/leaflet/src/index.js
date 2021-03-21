1e13dda368479afe86e8e7dcb0823bcfd3727a24
Anders <idris.maps@gmx.ch>
2021-03-21 16:09:15 +0100

    Mise en place leaflet

---
+++ modules/leaflet/src/index.js
@@ -0,0 +1,15 @@
+import L from 'leaflet'
+
+const map = L.map('map').setView([46.781048, 6.647136], 15)
+
+const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+})
+
+osm.addTo(map)
+
+const heigStRoch = L.marker([46.781048, 6.647136]).bindPopup('HEIG-VD St-Roch')
+
+heigStRoch.addTo(map)
+
